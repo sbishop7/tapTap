d57d1f48d5e4ee844c7a8cb20a33044838f43a52
sbishop7 <seth.bishop@gmail.com>
2017-07-13 11:20:13 -0700

    Copied content from TTT

---
+++ TapMaster/TapTapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TapTapViewController.swift
+//  TapMaster
+//
+//  Created by Seth Bishop on 7/13/17.
+//  Copyright © 2017 Seth Bishop. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class TapTapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        WinnerLabel.isHidden = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
