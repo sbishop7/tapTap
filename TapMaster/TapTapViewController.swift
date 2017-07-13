9267caeba3ad530f0c4e5a0a1fb17868b99a9e6a
John Colley <webjocky@gmail.com>
2017-07-13 14:49:22 -0700

    added gameNodes and node classes

--- TapMaster/TapTapViewController.swift
+++ TapMaster/TapTapViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import Foundation
-
 import UIKit
 
 class TapTapViewController: UIViewController {
+    var homeScore = 0
+    var awayScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        WinnerLabel.isHidden = true
+
+        gridRefresh(num: 4)
+    }
+    
+    func gridRefresh(num: Int) {
+        // Disable & Hide all buttons while we re-populate the grid
+        for view in self.view.subviews as [UIView] {
+            if let node = view as? gameNode {
+                node.isHidden = true
+                node.isEnabled = false
+            }
+        }
+        
+        //Populate the grid & enable nodes
+        for counter in 1...num {
+            let pos = getRandGridPosition(i: UInt32(counter))
+            let node = self.view.viewWithTag(pos) as? gameNode
+            node?.isEnabled = true
+            node?.isHidden = false
+            node?.setTitle(String(describing: type(of: node)), for: .normal)
+            print(node?.currentTitle!)
+        }
+    }
+    
+    func getRandGridPosition(i: UInt32) -> Int {
+        let pos = Int(arc4random_uniform(i) + 1)
+        return pos
     }
     
     override func didReceiveMemoryWarning() {
@@ -22,5 +49,7 @@ class TapTapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
