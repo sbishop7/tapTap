8f34bc8d9f458e431efa17107f98c797ea24012f
John Colley <webjocky@gmail.com>
2017-07-14 14:33:02 -0700

    removed irrelevant commented logic

--- TapMaster/GameSceneController.swift
+++ TapMaster/GameSceneController.swift
@@ -100,38 +100,3 @@ extension GameSceneController: MultiServiceManagerDelegate {
         print("received \(dictionary) in dictionarySent function (MSMDelegate)")
     }
 }
-
-
-//func gridRefresh(num: Int) {
-//    // Disable & Hide all buttons while we re-populate the grid
-//    for view in self.view.subviews as [UIView] {
-//        if let node = view as? gameNode {
-//            node.isHidden = true
-//            node.isEnabled = false
-//        }
-//    }
-//    
-//    //Populate the grid & enable nodes
-//    for counter in 1...num {
-//        let pos = getRandGridPosition(i: UInt32(counter))
-//        let node = self.view.viewWithTag(pos) as? gameNode
-//        node?.isEnabled = true
-//        node?.isHidden = false
-//        node?.setTitle(String(describing: type(of: node)), for: .normal)
-//        print(node?.currentTitle!)
-//    }
-//}
-//
-//func getRandGridPosition(i: UInt32) -> Int {
-//    let pos = Int(arc4random_uniform(i) + 1)
-//    return pos
-//}
-//
-//override func didReceiveMemoryWarning() {
-//    super.didReceiveMemoryWarning()
-//    // Dispose of any resources that can be recreated.
-//}
-//
-//override var prefersStatusBarHidden: Bool {
-//    return true
-//}
